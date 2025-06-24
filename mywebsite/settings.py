c17593cb89fb32f2085cc2abd87b11fdb8df9d8f
jevin13 <jevina131999@gmail.com>
2025-06-24 22:47:59 +0400

    updated config for contact details

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -131,3 +131,12 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")             # your Gmail
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_APP_PASSWORD")  # store it in .env
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
